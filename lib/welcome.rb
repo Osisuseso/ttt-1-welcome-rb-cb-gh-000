157212f546b452c02cb59ca2d9296d23417aaf19
Fauzi Sulaeman <fauzi_sulaeman99@apps.ipb.ac.id>
2018-05-17 14:47:07 +0000

    automatically backed up by learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,8 +1,5 @@
 # Edit this file to output "Welcome to Tic Tac Toe!"
 # You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
-<<<<<<< HEAD
 puts "Welcome to Tic Tac toe!"
-=======
-puts "Welcome to Tic Tac toe!"
->>>>>>> 87e10b69285a3020c0208d0e3ee063533fb821c4
+
